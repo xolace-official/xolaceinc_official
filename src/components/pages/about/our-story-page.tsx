20a45bdb7dcf73a6e0f478b3e3e849e81947e595
Emmanuel <emmsom506@gmail.com>
2026-02-21 16:32:41 +0000

    Refactor our story page

--- src/components/pages/about/our-story-page.tsx
+++ src/components/pages/about/our-story-page.tsx
@@ -1,11 +1,11 @@
-import ExistingPlatformsGap from "@/features/about/our-story/exsiting-platforms-gap";
-import OurRoot from "@/features/about/our-story/our-root";
-import OurDream from "@/features/about/our-story/our-dream";
-import OurStoryCTA from "@/features/about/our-story/our-story-cta";
-import StoryHero from "@/features/about/our-story/story-hero";
-import TheRealization from "@/features/about/our-story/the-realization";
-import WhatWeObserved from "@/features/about/our-story/what-we-observed";
 import RadialStats from "@/features/about/our-story/radial-stats";
+import {StoryHero} from "@/features/about/our-story/story-hero";
+import {TheRealization} from "@/features/about/our-story/the-realization";
+import {WhatWeObserved} from "@/features/about/our-story/what-we-observed";
+import {ExistingPlatformsGap} from "@/features/about/our-story/exsiting-platforms-gap";
+import {OurDream} from "@/features/about/our-story/our-dream";
+import {OurStoryCTA} from "@/features/about/our-story/our-story-cta";
+import {OurRoot} from "@/features/about/our-story/our-root";
 
 const OurStoryPage = () => {
   return (
@@ -18,9 +18,9 @@ const OurStoryPage = () => {
 
       <ExistingPlatformsGap />
 
-      <OurRoot />
-
       <OurDream />
+
+      <OurRoot />
 
       <OurStoryCTA />
     </div>
