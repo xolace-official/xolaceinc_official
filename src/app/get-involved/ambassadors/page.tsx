13822f300a43e6a2cf0e87fdf82b211809d2da11
Emmanuel <emmsom506@gmail.com>
2026-02-27 18:55:31 +0000

    Add get involved page

---
+++ src/app/get-involved/ambassadors/page.tsx
@@ -0,0 +1,8 @@
+import AmbassadorsPage from "@/components/pages/get-involved/ambassadors/ambassadors-page";
+
+const Ambassador = () => {
+  return(
+    <AmbassadorsPage/>
+  )
+}
+export default Ambassador;
