a889c1b718be2ec7f7110f10fead74c19d0de470
Emmanuel <emmsom506@gmail.com>
2026-03-06 12:54:44 +0000

    Add partner with us route

---
+++ src/app/get-involved/partner-with-us/page.tsx
@@ -0,0 +1,8 @@
+import PartnerWithUsPage from "@/components/pages/get-involved/partner-with-us";
+
+const PartnerWithUs = () => {
+  return(
+    <PartnerWithUsPage/>
+  )
+}
+export default PartnerWithUs;
